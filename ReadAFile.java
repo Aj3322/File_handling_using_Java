483a301f434fc4af943d34c4fab55085cb1a2e8d
AJAY KUMAR <114848454+Aj3322@users.noreply.github.com>
2023-10-16 19:01:00 +0530

    Update ReadAFile.java

--- ReadAFile.java
+++ ReadAFile.java
@@ -2,14 +2,14 @@ import java.io.*;
 
 public class ReadAFile{
     public static void main(String[] args) throws IOException {
-        try (FileReader fr = new FileReader("abc.txt")) {
+       FileReader fr = new FileReader("abc.txt")
           int line =0;
 
                  while((line=fr.read())>-1){
                     System.out.print((char)line);
                  } fr.close();
+                  System.out.println("Task done!");
         }
-        System.out.println("Task done!");
+       
 
     }
-}
